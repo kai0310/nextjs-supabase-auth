c31b923c0964ffe03b4fb595587c0b6c48defe3c
kai0310 <kn0310@outlook.jp>
2023-11-07 03:39:38 +0900

    feat: add Google OAuth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,6 +28,33 @@ export default function Login({
     return redirect('/')
   }
 
+  const googleSignIn = async () => {
+    'use server'
+
+    const origin = headers().get('origin')
+    
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+
+    const { data, error }  = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${origin}/auth/callback`,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    })
+
+    if (error) {
+      return redirect('/login?message=Could not authenticate user')
+    }
+
+    return redirect(data.url)
+  }
+
   const signUp = async (formData: FormData) => {
     'use server'
 
@@ -75,44 +102,67 @@ export default function Login({
         Back
       </Link>
 
-      <form
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+      <div>
+        <form
+          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+          action={signIn}
         >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
+            Sign In
+          </button>
+          <button
+            formAction={signUp}
+            className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+          >
+            Sign Up
+          </button>
+          {searchParams?.message && (
+            <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+              {searchParams.message}
+            </p>
+          )}
+        </form>
+
+        <div className="relative my-5">
+          <div className="absolute inset-0 flex items-center" aria-hidden="true">
+            <div className="w-full border-t border-gray-200" />
+          </div>
+          <div className="relative flex justify-center text-sm font-medium leading-6">
+            <span className="bg-background px-6 text-gray-900">Or continue with</span>
+          </div>
+        </div>
+
+        <form
+          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+          action={googleSignIn}
+        >
+          <button
+            formAction={googleSignIn}
+            className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 w-full"
+          >
+            Google Login
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
